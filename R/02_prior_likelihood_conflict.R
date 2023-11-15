5d3b6bfb172af52a6841680501a7ef13a7e98eaa
Jesús N. Pinto-Ledezma <jesuspintoledezma@gmail.com>
2023-11-15 12:16:40 -0600

    update script 2

--- R/02_prior_likelihood_conflict.R
+++ R/02_prior_likelihood_conflict.R
@@ -118,3 +118,107 @@ curve(dnorm(x, 10, 1), add = TRUE, lty = 2, lwd = xlwd) # prior
 
 text(0, 0.42, "likelihood")
 text(10, 0.42, "prior")
+
+##### This chunk was extracted from the book: Regressions and Other Stories ##### 
+# You can download a copy of the book from here: https://avehtari.github.io/ROS-Examples/
+
+### Background 
+# Suppose an election is coming up, and a previously fitted model using economic and political conditions gives a forecast 
+# that the Democratic candidate will receive 52.4% of the two-party vote, with a predictive uncertainty of 4.1%. 
+# Using the notation above, theta-prior = 0.524 and se-prior = 0.041.
+# We now conduct a survey of 400 people, of whom 190 say they will vote for the Democratic
+# candidate and 210 support the Republican. If the survey was a simple random sample of voters
+# with no nonresponse, with voters who will not change their minds between the time of the
+# survey and the election, then the data estimate is theta-data = 190/400 = 0.475 with standard error
+# sqrt(0.475 ∗ (1 − 0.475)/400 = 0.025).
+
+
+#' Prior based on a previously-fitted model using economic and political condition.
+theta_hat_prior <- 0.524
+se_prior <- 0.041
+
+#' Survey of 400 people, of whom 190 say they will vote for the Democratic candidate
+n <- 400
+y <- 190 
+
+#' #### Data estimate
+theta_hat_data <- y/n 
+theta_hat_data
+
+se_data <- sqrt((y/n)*(1-y/n)/n) 
+se_data
+
+#' #### Bayes estimate
+theta_hat_bayes <- (theta_hat_prior/se_prior ^ 2 + theta_hat_data/se_data ^ 2) / (1/se_prior ^ 2 + 1/se_data ^ 2) 
+theta_hat_bayes
+
+se_bayes <- sqrt(1/(1/se_prior ^ 2 + 1/se_data ^ 2))
+se_bayes
+
+#' ## Figures
+
+# Prior versus Likelihood
+par(mar = c(3, 1, 1, 1), mgp = c(1.5, 0.5, 0), tck = -.02)
+
+plot(0, 0, xlim = c(0.37, 0.67), 
+     ylim = c(0, 20), 
+     xlab = expression(theta), 
+     xaxt = "n", ylab = "", yaxs = "i", yaxt = "n", bty = "n", cex.lab = 1.2)
+
+axis(1, seq(0.3, 0.7, 0.1))
+curve(dnorm(x, theta_hat_prior, se_prior), n = 1000, add = TRUE, col = "red")
+text(0.588, 5, "Prior")
+curve(dnorm(x, theta_hat_data, se_data), n = 1000, add = TRUE, col = "blue")
+text(0.420, 8, "Likelihood")
+
+# In this example, the prior standard error is 4.1% and the data standard error is 2.5%, 
+# so the data are more informative than the prior, 
+# and the Bayes estimate will be closer to the data.
+
+# Indeed - the posterior or Bayes estimation is between the prior and data estimates, but closer to the data.
+
+# Prior versus Likelihood versus Posterior
+plot(0, 0, xlim = c(0.37, 0.67), 
+     ylim = c(0, 20), 
+     xlab = expression(theta), 
+     xaxt = "n", ylab = "", yaxs = "i", yaxt = "n", bty = "n", cex.lab = 1.2) 
+
+axis(1, seq(0.3, 0.7, 0.1))
+curve(dnorm(x, theta_hat_prior, se_prior), n = 1000, add = TRUE, col = "red")
+text(0.588, 5, "Prior")
+curve(dnorm(x, theta_hat_data, se_data), n = 1000, add = TRUE, col = "blue")
+text(0.420, 8, "Likelihood")
+curve(dnorm(x, theta_hat_bayes, se_bayes), n = 1000, add = TRUE, col = "green")
+text(0.525, 15, "Posterior")
+
+# Now consider the same situation but with much more uncertain data. 
+# Just take the same point estimate and increase the data standard error se-data from 0.025 to, say, 0.075. 
+# The Bayes estimate then becomes 0.512; it is now closer to the prior.
+
+#' #### Data estimate 
+se_data2 <- 0.075
+se_data2
+
+#' #### Bayes estimate
+theta_hat_bayes2 <- (theta_hat_prior/se_prior ^ 2 + theta_hat_data/se_data2 ^ 2) / (1/se_prior ^ 2 + 1/se_data2 ^ 2) 
+theta_hat_bayes2
+
+se_bayes2 <- sqrt(1/(1/se_prior ^ 2 + 1/se_data2 ^ 2))
+se_bayes2
+
+### Figure 
+# Prior versus Likelihood versus Posterior versus Posterior 2
+plot(0, 0, xlim = c(0.37, 0.67), 
+     ylim = c(0, 20), 
+     xlab = expression(theta), 
+     xaxt = "n", ylab = "", yaxs = "i", yaxt = "n", bty = "n", cex.lab = 1.2) 
+
+axis(1, seq(0.3, 0.7, 0.1))
+curve(dnorm(x, theta_hat_prior, se_prior), n = 1000, add = TRUE, col = "red")
+text(0.588, 5, "Prior")
+curve(dnorm(x, theta_hat_data, se_data), n = 1000, add = TRUE, col = "blue")
+text(0.420, 8, "Likelihood")
+curve(dnorm(x, theta_hat_bayes, se_bayes), n = 1000, add = TRUE, col = "green")
+text(0.525, 15, "Posterior - low uncertainty")
+curve(dnorm(x, theta_hat_bayes2, se_bayes2), n = 1000, add = TRUE, col = "purple")
+text(0.525, 11, "Posterior - high uncertainty")
